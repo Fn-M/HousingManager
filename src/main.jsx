92cd6e73c755b39f79b2fdaaf0b968b7c9d03d15
Fn-M <96444305+Fn-M@users.noreply.github.com>
2025-10-14 20:43:03 +0200

    Update the BrowseRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Define o basename baseado no ambiente
+const basename = import.meta.env.MODE === 'production' ? '/HousingManager' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
